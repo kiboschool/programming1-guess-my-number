6f862f7e98a24bdf9c89181218755218dc88b357
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 02:53:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# See the Instructions tab
+
+# Set secret number
+
+print("I'm thinking of a number between 1 and 99")
+
+# Ask the user to guess
+# Check to see if the guess is <, >, or = secret number
+# Print the right message
