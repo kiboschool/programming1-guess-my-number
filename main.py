3a148ac950ea3719ba62fcac57a3bb4b842eb3b1
Emeka Okonkwo <chukwuemekaokonkwo24@gmail.com>
2026-04-22 18:20:04 +0100

    guess my number

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # Write your code here
-
+import random
 # Set secret number
-
+secret_number = random.randint(1,100)
 print("I'm thinking of a number between 1 and 99")
-
 # Ask the user to guess
+
+while True:
+    guess = input("Enter a guess: ")
+    if not guess.isdecimal():
+        print("That's NOT a number")
+        continue
+
+    guess = int(guess)
+    if guess == secret_number:
+        print("You are a genius! That's correct!")
+        break
+    elif guess < secret_number:
+        print("Your guess is too low")
+    else:
+        print("Your guess is too high")
+
 # Check to see if the guess is <, >, or = secret number
 # Print the right message
